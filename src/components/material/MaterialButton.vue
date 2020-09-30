e8fc96afe9f086b61bd844c1278ceee63c776c37
Ville Takanen <vitkukissa@gmail.com>
2020-09-30 10:06:01 +0300

    feat: material button

---
+++ src/components/material/MaterialButton.vue
@@ -0,0 +1,52 @@
+<template>
+  <button :class="buttonClasses" @click="clicked">
+    <slot />
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  props: {
+    fab: {
+      type: String,
+      required: false,
+      default: undefined
+    },
+    icon: {
+      type: String,
+      required: false,
+      default: undefined
+    },
+    text: {
+      type: String,
+      required: false,
+      default: undefined
+    },
+    action: {
+      type: Function,
+      required: false
+    }
+  },
+  setup (props) {
+    const buttonClasses = ref(
+      {
+        'material-button': true,
+        'material-fab': false,
+        'material-button-text': false,
+        'material-button-icon': false
+      }
+    )
+    if (typeof props.text !== 'undefined') buttonClasses.value['material-button-text'] = true
+    if (typeof props.fab !== 'undefined') buttonClasses.value['material-fab'] = true
+    else if (typeof props.icon !== 'undefined') buttonClasses.value['material-button-icon'] = true
+
+    const clicked = () => {
+      if (props.action) props.action()
+    }
+
+    return { buttonClasses, clicked }
+  }
+})
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <MaterialButton>Just a button?</MaterialButton>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import MaterialButton from '@/components/material/MaterialButton.vue' // @ is an alias to /src
 
 export default defineComponent({
   name: 'Home',
   components: {
-    HelloWorld
+    MaterialButton
   }
 })
 </script>
