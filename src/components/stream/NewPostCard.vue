3e70c8e3a8a356313948fd35be7d437649ee2c81
Ville Takanen <vitkukissa@gmail.com>
2020-09-30 14:56:08 +0300

    feat: new post component

---
+++ src/components/stream/NewPostCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <MaterialCard>
+    <p v-if="!isAuthz">Please <a href="/login">login</a> to post</p>
+    <textarea v-model="newPostContent"></textarea>
+    {{newPostContent}}
+    <MaterialButton :disabled="isAuthz">Post!</MaterialButton>
+  </MaterialCard>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import MaterialButton from '@/components/material/MaterialButton.vue'
+import MaterialCard from '@/components/material/MaterialCard.vue'
+import { useAuthz } from '@/lib/authz'
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    MaterialButton,
+    MaterialCard
+  },
+  setup () {
+    const { isAuthz } = useAuthz()
+    const newPostContent = ref('')
+    return { isAuthz, newPostContent }
+  }
+})
+</script>
+
+<style lang="sass" scoped>
+textarea
+ display: block
+ width: 100%
+ height: 5em
+ border: none
+ resize: none
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
     <img class="fox-logo" alt="Pelilauta" src="../assets/fox.svg">
     <MaterialButton>Just a button?</MaterialButton>
   </MaterialCard>
+  <NewPostCard />
   <div class="two-columns">
     <MaterialCard>
       <h3>Stream</h3>
@@ -19,13 +20,15 @@ import { defineComponent } from 'vue'
 import MaterialButton from '@/components/material/MaterialButton.vue'
 import MaterialCard from '@/components/material/MaterialCard.vue'
 import Stream from '@/components/stream/Stream.vue'
+import NewPostCard from '@/components/stream/NewPostCard.vue'
 
 export default defineComponent({
   name: 'Home',
   components: {
     MaterialButton,
     MaterialCard,
-    Stream
+    Stream,
+    NewPostCard
   }
 })
 </script>
