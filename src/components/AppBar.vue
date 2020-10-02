50729f127c7ea11e54ffd9ec0ed928f8cdc8e279
Ville Takanen <vitkukissa@gmail.com>
2020-10-02 09:21:17 +0300

    fix(theme): color updates

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -7,8 +7,11 @@
       class="toolbar"
     >
       <h1 class="font-button">
-        Pelilauta
+        Pelilauta 2
       </h1>
+      <div style="vertical-align:top; height 30px">
+        <p  style="line-height: 14px; font-size: 14px; color: #adc">α</p>
+      </div>
       <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
       <div class="spacer" />
