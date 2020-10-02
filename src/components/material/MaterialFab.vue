cec9d65e3851d303574f2b81fd3c79be640bd4ae
Ville Takanen <vitkukissa@gmail.com>
2020-10-02 14:16:33 +0300

    fix(ux): fab responsive position

---
+++ src/components/material/MaterialFab.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="material-fab-container">
+    <div class="material-fab" @click="clicked">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useRouter } from 'vue-router'
+export default defineComponent({
+  name: 'MaterialFab',
+  props: {
+    to: {
+      type: String,
+      required: false
+    },
+    action: {
+      type: Function,
+      required: false
+    }
+  },
+  setup (props) {
+    const router = useRouter()
+
+    function clicked () {
+      if (props.to) router.push(props.to)
+      else if (props.action) props.action()
+    }
+
+    return { clicked }
+  }
+})
+</script>
+
+<style lang="sass">
+@import @/styles/include-media.scss
+@import @/styles/material-colors.sass
+@import @/styles/material-typography.sass
+
+.material-fab-container
+  z-index: 50000
+  position: fixed
+  bottom: 16px
+  right: 16px
+  height: 56px
+  width: 56px
+  margin: 0
+  padding: 0
+  .material-fab
+    @include ElementColorSecondary()
+    @include BoxShadow12()
+    position: relative
+    height: 56px
+    width: 56px
+    margin: 0
+    border-radius: 50%
+    text-align: center
+    img
+      height: 44px
+      width: 44px
+      margin: 6px
+
+@include media('>1024px')
+  .material-fab-container
+    left:50%
+    transform: translate(354px, 0)
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <NewPostCard />
   <Stream />
+  <MaterialFab>
+    <img src="@/assets/add.svg" alt="new comment" />
+  </MaterialFab>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import Stream from '@/components/stream/Stream.vue'
 import NewPostCard from '@/components/stream/NewPostCard.vue'
+import MaterialFab from '@/components/material/MaterialFab.vue'
 
 export default defineComponent({
   name: 'Home',
   components: {
     Stream,
-    NewPostCard
+    NewPostCard,
+    MaterialFab
   }
 })
 </script>
