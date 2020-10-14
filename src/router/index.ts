ecd077b8050b23b316f5a6a77bacdf5bd9e43ca0
Ville Takanen <vitkukissa@gmail.com>
2020-10-14 14:57:07 +0300

    fix(post): remove streampost view

--- src/router/index.ts
+++ src/router/index.ts
@@ -60,16 +60,6 @@ const routes: Array<RouteRecordRaw> = [
     props: true
   },
   {
-    path: '/stream/post',
-    name: 'StreamPost',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/StreamPost.vue'),
-    children: [{ path: ':topic', component: () => import(/* webpackChunkName: "about" */ '../views/StreamPost.vue') }],
-    props: true
-  },
-  {
     path: '/stream/view/:postid',
     name: 'StreamView',
     // route level code-splitting
