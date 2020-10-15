5eadf65fb00b7fe29552344c7e414771684d8792
Ville Takanen <vitkukissa@gmail.com>
2020-10-15 22:10:42 +0300

    fix(editor): default topic restored

--- src/components/editor/EditorDialog.vue
+++ src/components/editor/EditorDialog.vue
@@ -125,7 +125,7 @@ export default defineComponent({
     const router = useRouter()
 
     const content = ref('')
-    const chosenTopic = ref('')
+    const chosenTopic = ref('Yleinen')
     const images = ref(new Array<string>())
 
     const title = ref('')
