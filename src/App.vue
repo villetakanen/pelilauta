9c4cb07de1f8826317daa6364f3016fc0a0ca1b1
Ville Takanen <vitkukissa@gmail.com>
2020-10-19 12:39:50 +0300

    fix(identity): about page updates

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,21 @@
     <MaterialBanner />
     <main>
       <router-view />
-      <div style="text-align: center; padding-bottom:16px">
+      <div style="text-align: center; padding:16px" class="footnote">
         <span style="line-height: 56px;opacity:0.37; font-size:12px">Pelilauta</span>
         <img
           style="max-height: 56px; max-width:96px; vertical-align: middle"
           src="@/assets/fox.svg"
         >
         <span style="line-height: 56px;opacity:0.37; font-size:12px">{{ version }} {{ $t('lang') }}</span>
+        <div style="text-align: center; padding:16px; line-height: 12px;opacity:0.37; font-size:12px">
+          <router-link to="/about">
+            {{ $t('app.footnote') }}
+          </router-link>
+        </div>
+        <div style="text-align: center; padding:16px; line-height: 12px;opacity:0.37; font-size:12px">
+          <a href="https://https://github.com/villetakanen/pelilauta">GitHub</a>
+        </div>
       </div>
     </main>
   </div>
@@ -54,4 +62,8 @@ export default defineComponent({
 
 main
   padding-top: 62px
+
+.footnote
+  a
+    text-decoration: none
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,18 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <p>Back to <a href="/">home</a></p>
-    <p>
-      <router-link to="/editortest">
-        testing page
-      </router-link>
-    </p>
-  </div>
+  <MaterialCard class="about">
+    <h1>{{ $t('about.title') }}</h1>
+    <p>{{ $t('about.text') }}</p>
+  </MaterialCard>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import MaterialCard from '@/components/material/MaterialCard.vue'
+
+export default defineComponent({
+  name: 'About',
+  components: {
+    MaterialCard
+  }
+})
+</script>
