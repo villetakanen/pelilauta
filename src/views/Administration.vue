d1fe2cce8ae366ec54320790fda85ad37d092cc0
Ville Takanen <vitkukissa@gmail.com>
2020-10-23 15:12:30 +0300

    fix(editor): removed deprecated code

--- src/views/Administration.vue
+++ src/views/Administration.vue
@@ -12,20 +12,10 @@
       <a @click="dialog=!dialog">Show Dialog</a>
     </p>
   </MaterialCard>
-  <Dialog
-    v-model="dialog"
-  >
-    <!-- MaterialCard>
-      <h1>Dialog!</h1>
-      <p>Hmm, it seems your account is frozen? Maybe you should contact the admins for more info.</p>
-    </MaterialCard -->
-    <EditorCard @close-dialog="dialog=false" />
-  </Dialog>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
-import EditorCard from '@/components/editor/EditorCard.vue'
 import Dialog from '@/components/material/Dialog.vue'
 import MaterialCard from '@/components/material/MaterialCard.vue'
 import UserList from '@/components/UserList.vue'
@@ -33,9 +23,7 @@ import UserList from '@/components/UserList.vue'
 export default defineComponent({
   name: 'Admin',
   components: {
-    EditorCard,
     UserList,
-    Dialog,
     MaterialCard
   },
   setup () {
