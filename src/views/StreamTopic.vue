1807cc14b3dcc1f5a7ed4e217a91ef31c9c14067
Ville Takanen <vitkukissa@gmail.com>
2020-11-02 11:52:55 +0200

    fix: router onMounted

--- src/views/StreamTopic.vue
+++ src/views/StreamTopic.vue
@@ -18,10 +18,9 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, ref, watch } from 'vue'
+import { computed, defineComponent, onMounted, ref, watch } from 'vue'
 import Stream from '@/components/stream/Stream.vue'
 import MaterialFab from '@/components/material/MaterialFab.vue'
-import MaterialCard from '@/components/material/MaterialCard.vue'
 import { useMeta } from '@/lib/meta'
 import { useEditorDialog } from '@/lib/editor'
 import { useRoute } from 'vue-router'
@@ -40,14 +39,26 @@ export default defineComponent({
   },
   setup (props) {
     const { topics } = useMeta()
+
     const route = useRoute()
-    const routeTopic = ref(route.params.topic as string)
-    watch(route, (val) => { routeTopic.value = val.params.topic as string })
+    const routeTopic = ref('')
+    onMounted(() => {
+      if (route?.params?.topic) routeTopic.value = route.params.topic as string
+      watch(route, (val) => {
+        if (!route.params?.topic) {
+          // eslint-disable-next-line
+          console.warn('no topic')
+        }
+        routeTopic.value = val.params.topic as string
+      })
+    })
+
     const pageTitle = computed(() => {
       const t = topics.value.find((val) => (val.slug.toLowerCase() === routeTopic.value.toLowerCase()))
       if (t) return t
       return { title: '-' }
     })
+
     const { showStreamActions } = useMeta()
 
     const newPostDialog = () => {
