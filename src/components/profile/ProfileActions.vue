80154913cd5d6a2378f5b9f1f64c922f49e4d882
Ville Takanen <vitkukissa@gmail.com>
2020-11-09 14:13:55 +0200

    fix: login and logout redirects

---
+++ src/components/profile/ProfileActions.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="toolbar">
+    <div class="spacer" />
+    <!-- MaterialButton text>
+      {{ $t('actions.forgetMe') }}
+    </MaterialButton-->
+    <MaterialButton :action="logout">
+      {{ $t('action.logout') }}
+    </MaterialButton>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import MaterialButton from '@/components/material/MaterialButton.vue'
+import { useRouter } from 'vue-router'
+// import { useAuthState } from '@/state/authz'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
+export default defineComponent({
+  name: 'ProfileActions',
+  components: {
+    MaterialButton
+  },
+  setup () {
+    const router = useRouter()
+
+    const logout = () => {
+      firebase.auth().signOut().then(() => {
+        router.push('/')
+      })
+    }
+
+    return { logout }
+  }
+})
+</script>
