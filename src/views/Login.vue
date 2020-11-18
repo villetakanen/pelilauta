84f6c6e7837b759c5b80be17d3e1e62d22899e7d
Ville Takanen <vitkukissa@gmail.com>
2020-11-18 13:12:20 +0200

    fix: login screen ux and code quality fixes

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,8 +1,11 @@
 <template>
+  <ViewHeader>
+    {{ $t('login.title') }}
+  </ViewHeader>
   <div class="contentGrid">
     <MaterialCard class="main-view">
-      <p>Please login or register to Pelilauta with the buttons below.</p>
-      <div style="text-align: center">
+      <p>{{ $t('login.message') }}</p>
+      <div class="loginButton">
         <MaterialButton
           :action="socialGoogleLogin"
           class="googleLoginButton"
@@ -11,10 +14,10 @@
             src="../assets/google.svg"
             alt="G"
             class="icon"
-          > Login with Google
+          > {{ $t('login.withGoogleButton') }}
         </MaterialButton>
       </div>
-      <div style="text-align: center">
+      <div class="loginButton">
         <MaterialButton
           :action="socialFacebookLogin"
           class="facebookLoginButton"
@@ -23,7 +26,7 @@
             src="../assets/facebook.svg"
             alt="F"
             class="icon"
-          > Login with Facebook
+          > {{ $t('login.withFacebookButton') }}
         </MaterialButton>
       </div>
     </MaterialCard>
@@ -32,17 +35,19 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-
 import firebase from 'firebase/app'
 import 'firebase/auth'
-import MaterialCard from '../components/material/MaterialCard.vue'
-import MaterialButton from '../components/material/MaterialButton.vue'
+import 'firebase/analytics'
+import MaterialCard from '@/components/material/MaterialCard.vue'
+import MaterialButton from '@/components/material/MaterialButton.vue'
+import ViewHeader from '@/components/app/ViewHeader.vue'
 import { useRouter } from 'vue-router'
 
 export default defineComponent({
   components: {
     MaterialButton,
-    MaterialCard
+    MaterialCard,
+    ViewHeader
   },
   setup () {
     const router = useRouter()
@@ -51,6 +56,7 @@ export default defineComponent({
       const provider = new firebase.auth.GoogleAuthProvider()
       provider.addScope('email')
       firebase.auth().signInWithPopup(provider).then(() => {
+        firebase.analytics().logEvent('Login', { vendor: 'Google' })
         router.push('/')
       })
     }
@@ -59,6 +65,7 @@ export default defineComponent({
       const provider = new firebase.auth.FacebookAuthProvider()
       provider.addScope('email')
       firebase.auth().signInWithPopup(provider).then(() => {
+        firebase.analytics().logEvent('Login', { vendor: 'Facebook' })
         router.push('/')
       })
     }
@@ -70,16 +77,20 @@ export default defineComponent({
 
 <style lang="sass">
 #app
+  .loginButton
+    padding: 8px
+    margin: 0 auto
+    text-align: center
   .googleLoginButton
     background-color: white
     color: #666
-    width: 240px
+    width: 230px
     text-align: left
 
   .facebookLoginButton
     background-color: #4267B2
     color: rgba(255,255,255,0.8)
-    width: 240px
+    width: 230px
     text-align: left
 
 </style>
