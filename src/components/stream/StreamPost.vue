a3ac566226e83b02087e6c4b54f951b76ec98d3c
Ville Takanen <vitkukissa@gmail.com>
2020-11-30 14:01:34 +0200

    feat: card graphics demo

--- src/components/stream/StreamPost.vue
+++ src/components/stream/StreamPost.vue
@@ -122,6 +122,19 @@ export default defineComponent({
 <style lang="sass" scoped>
 @import ../../styles/material-typography.sass
 @import @/styles/material-colors.sass
+.stream-post
+  overflow: hidden
+
+.stream-post-content:before
+  content: ''
+  position: absolute
+  background-image: url('../../assets/icons-in-primary/d12-primary.svg')
+  background-size: 200px 200px
+  height: 200px
+  width: 200px
+  bottom: -52px
+  right: -33px
+  opacity: 0.11
 
 .stream-post-content
   @include TypeBody2()
