640f4fcdeda47c0f8696d007ce01d58262fefb3e
Ville Takanen <vitkukissa@gmail.com>
2020-12-14 13:23:21 +0200

    feat: restored new thread button to frontpage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,34 @@
   <div class="contentGrid">
     <Stream />
   </div>
+  <teleport to="body">
+    <Fab
+      id="addThreadFab"
+      :action="newThread"
+      :text="$t('action.addThread')"
+    >
+      <img
+        src="@/assets/icons/add.svg"
+        alt="new post"
+      >
+    </Fab>
+  </teleport>
+  <EditorDialog v-model="showEditorDialog" />
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, onMounted } from 'vue'
 import Stream from '@/components/stream/Stream.vue'
-import MaterialFab from '@/components/material/MaterialFab.vue'
 import { useMeta } from '@/lib/meta'
+import Fab from '@/components/material/Fab.vue'
+import EditorDialog from '@/components/app/EditorDialog.vue'
 
 export default defineComponent({
   name: 'Home',
   components: {
-    Stream
+    Stream,
+    Fab,
+    EditorDialog
   },
   setup () {
     const { showStreamActions } = useMeta()
@@ -21,7 +37,26 @@ export default defineComponent({
     onMounted(() => {
       document.title = 'Pelilauta'
     })
-    return { showStreamActions, editorDialog }
+    const showEditorDialog = ref(false)
+
+    const newThread = () => {
+      console.log('newThread')
+      showEditorDialog.value = true
+    }
+    return { showStreamActions, editorDialog, showEditorDialog, newThread }
   }
 })
 </script>
+
+<style lang="sass">
+@import @/styles/include-media.scss
+@import @/styles/material-colors.sass
+@import @/styles/material-typography.sass
+
+#addThreadFab
+  @include BoxShadow8()
+  position: fixed
+  left: 50%
+  transform: translateX(-50%)
+  bottom: 16px
+</style>
