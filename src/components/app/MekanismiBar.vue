7c96c2aaa8857cc1ae52618480b05ffd060462a2
Ville Takanen <vitkukissa@gmail.com>
2021-01-19 14:49:38 +0200

    fix: link styles for mekanismi bar

--- src/components/app/MekanismiBar.vue
+++ src/components/app/MekanismiBar.vue
@@ -1,6 +1,21 @@
 <template>
-  <div id="mekanismiBar">...</div>
+  <div id="mekanismiBar">
+    <router-link to="/mekanismi"> site.name </router-link>&nbsp;/&nbsp;<router-link to="/mekanismi"> page.name </router-link>
+  </div>
+  <div id="mekanismiBarSpacer" />
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup () {
+    /* const site = inject('site') as Site
+    const page = inject('page') as Page
+    return { site, page } */
+  }
+})
+</script>
 
 <style lang="sass" scoped>
 @import @/styles/include-media.scss
@@ -8,7 +23,20 @@
 @import @/styles/material-typography.sass
 
 #mekanismiBar
-  margin-top: 56px
-  height: 24px
+  @include TypeCaption()
+  position: absolute
+  left: 0
+  top: 56px
+  height: 22px
   background-color: $color-fill-tertiary
+  width: calc(100vw - 32px)
+  padding: 2px 16px
+  line-height: 22px
+  display: flex
+  color: #ccc
+  a
+    color: white
+    text-decoration: none
+#mekanismiBarSpacer
+  height: 20px
 </style>
