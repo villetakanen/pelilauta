5b5b6affa8d16363e58ae88385df054ac940eca6
Ville Takanen <vitkukissa@gmail.com>
2021-01-22 11:59:22 +0200

    fix: added missing type exports

--- src/state/authz/index.ts
+++ src/state/authz/index.ts
@@ -1,7 +1,8 @@
-import { useProfile, PublicProfile } from './profile'
+import { useProfile, PublicProfile, ProfileMeta } from './profile'
 import { useAuthState } from './state'
 
 export {
+  ProfileMeta,
   PublicProfile,
   useAuthState,
   useProfile
