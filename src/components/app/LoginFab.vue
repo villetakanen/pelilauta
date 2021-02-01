96a0d1e98fc0bea30986ed35cde57070732a6911
Ville Takanen <vitkukissa@gmail.com>
2021-02-01 15:43:51 +0200

    feat: login fab skeleton

---
+++ src/components/app/LoginFab.vue
@@ -0,0 +1,27 @@
+<template>
+  <Fab
+    v-if="false"
+    class="loginFab"
+  >
+    <span class="loginBG" />
+    <span>{{ $t('action.login') }}</span>
+    <span>{{ $t('action.register') }}</span>
+  </Fab>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Fab from '../material/Fab.vue'
+
+export default defineComponent({
+  name: 'LoginFab',
+  components: { Fab }
+})
+</script>
+
+<style lang="sass" scoped>
+@import @/styles/include-media.scss
+@import @/styles/material-colors.sass
+@import @/styles/material-typography.sass
+
+</style>
