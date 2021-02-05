8c120bc4802cde533c25b6165ae6af7a0d6ef9d9
Ville Takanen <vitkukissa@gmail.com>
2021-02-05 08:53:49 +0200

    feat: cache firebase images

--- public/service-worker.js
+++ public/service-worker.js
@@ -19,3 +19,33 @@ self.addEventListener('message', (event) => {
     }
   })
 
+let CACHE_NAME = 'pelilauta-images-cache'
+self.addEventListener("fetch", function(e) {
+  e.respondWith((async function() {
+    const cachedResponse = await caches.match(e.request)
+    if (cachedResponse) {
+      return cachedResponse
+    }
+
+    const networkResponse = await fetch(e.request)
+
+    const hosts = [
+      'https://fonts.googleapis.com',
+      'https://firebasestorage.googleapis.com'
+    ]
+
+    if (hosts.some((host) => e.request.url.startsWith(host))) {
+      // This clone() happens before `return networkResponse` 
+      const clonedResponse = networkResponse.clone();
+
+      e.waitUntil((async function() {
+        const cache = await caches.open(CACHE_NAME)
+        // This will be called after `return networkResponse`
+        // so make sure you already have the clone!
+        await cache.put(e.request, clonedResponse)
+      })())
+    }
+
+    return networkResponse
+  })())
+})
