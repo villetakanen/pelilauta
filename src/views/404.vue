f0829953a04785f783539dd2b54abc12502fff45
Ville Takanen <vitkukissa@gmail.com>
2021-02-07 12:57:06 +0200

    feat: 404 page prototype

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,26 +1,30 @@
 <template>
   <div class="errorView">
+    <Toolbar>
+      <h3>{{ $t('errorPage.notFound') }}</h3>
+    </Toolbar>
     <div class="pageAttentionPic" />
   </div>
 </template>
 
 <script lang="ts">
+import Toolbar from '@/components/layout/Toolbar.vue'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
+  components: { Toolbar }
 })
 </script>
 
 <style lang="sass" scoped>
 .errorView
-  min-height: calc(100vh - 64px)
   position: relative
   .pageAttentionPic
     position: fixed
     bottom: 0
-    left: 33%
+    left: 22%
     height: calc(80vh)
-    width: calc(22vw)
+    width: calc(33vw)
     background-repeat: no-repeat
     background-position: bottom
     background-image: url('/src/404-element.svg')
