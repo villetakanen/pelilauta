7e62d69dad10a972e56706be0430fe77d208c8c7
Ville Takanen <vitkukissa@gmail.com>
2021-02-08 11:39:20 +0200

    fix: 404 styles on mobile

--- src/views/404.vue
+++ src/views/404.vue
@@ -3,23 +3,47 @@
     <Toolbar>
       <h3>{{ $t('errorPage.notFound') }}</h3>
     </Toolbar>
+    <MaterialCard class="errorCard">
+      <h1>:/</h1>
+      <p>{{ $t('errorPage.message') }}</p>
+      <Toolbar>
+        <div class="spacer" />
+        <MaterialButton
+          text
+          to="/"
+        >
+          {{ $t('siteInfo.title') }}
+        </MaterialButton>
+      </Toolbar>
+    </MaterialCard>
     <div class="pageAttentionPic" />
   </div>
 </template>
 
 <script lang="ts">
 import Toolbar from '@/components/layout/Toolbar.vue'
+import MaterialButton from '@/components/material/MaterialButton.vue'
+import MaterialCard from '@/components/material/MaterialCard.vue'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  components: { Toolbar }
+  components: { Toolbar, MaterialCard, MaterialButton }
 })
 </script>
 
 <style lang="sass" scoped>
+@import @/styles/include-media.scss
+@import @/styles/material-colors.sass
+@import @/styles/material-typography.sass
+
+.errorCard
+  width: 40%
+  margin-left: 50%
+
 .errorView
   position: relative
   .pageAttentionPic
+    z-index: -1
     position: fixed
     bottom: 0
     left: 22%
@@ -28,4 +52,25 @@ export default defineComponent({
     background-repeat: no-repeat
     background-position: bottom
     background-image: url('/src/404-element.svg')
+
+@include media('<tablet')
+  .errorCard
+    width: calc(100% - 48px)
+    margin-left: 8px
+    margin-right: 8px
+    opacity: 0.77
+    backdrop-filter: blur(1px)
+  .errorView
+    position: relative
+    .pageAttentionPic
+      opacity: 1
+      position: fixed
+      bottom: 0
+      left: 16px
+      height: calc(80vh)
+      width: calc(80vw)
+      background-repeat: no-repeat
+      background-position: bottom
+      background-image: url('/src/404-element.svg')
+
 </style>
