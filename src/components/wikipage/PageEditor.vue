e20438663fb4001c6b672eaf6b40658677bcf028
Ville Takanen <vitkukissa@gmail.com>
2021-02-24 11:03:29 +0200

    feat: refactored wiki page editor to new view/comp

---
+++ src/components/wikipage/PageEditor.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>{{ site }}</div>
+  <hr>
+  <div>{{ page }}</div>
+</template>
+
+<script lang="ts">
+import { Page, Site } from '@/state/site'
+import { defineComponent, PropType } from 'vue'
+
+/**
+ * An editor form for a Wiki Page.
+ */
+export default defineComponent({
+  name: 'ThreadEditor',
+  components: {
+  },
+  props: {
+    site: {
+      type: Object as PropType<Site>,
+      required: true
+    },
+    page: {
+      type: Object as PropType<Page>,
+      required: true
+    }
+  }
+})
+</script>
---
+++ src/views/wiki/EditPage.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="editPage contentGrid">
+    <MaterialCard>
+      ...
+    </MaterialCard>
+  </div>
+</template>
+
+<script lang="ts">
+import MaterialCard from '@/components/material/MaterialCard.vue'
+import { usePages, useSite } from '@/state/site'
+import { defineComponent } from 'vue'
+
+/**
+ * A Router view for a Wiki Page in an edit mode.
+ *
+ * Loads all the required State entities, and initiates the required Firebase
+ * subscriptions.
+ *
+ * Does not contain any functionality aside from state management and component
+ * import/layout
+ */
+export default defineComponent({
+  name: 'EditPage',
+  components: {
+    MaterialCard
+  },
+  props: {
+    siteid: {
+      type: String,
+      required: true
+    },
+    pageid: {
+      type: String,
+      required: true
+    }
+  },
+  setup () {
+    const { site } = useSite()
+    const { page } = usePages()
+    return { site, page }
+  }
+})
+</script>
