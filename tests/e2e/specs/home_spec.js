7e9208fba74120e626e96366e99e5a3c59627e92
Ville Takanen <vitkukissa@gmail.com>
2021-03-10 22:31:10 +0200

    fix: service worker and caches disabled for cypres

---
+++ tests/e2e/specs/home_spec.js
@@ -0,0 +1,29 @@
+before(function () {
+  // run this once before all code
+  return window.caches.keys().then(function (cacheNames) {
+    return Promise.all(
+      cacheNames.map(function (cacheName) {
+        return window.caches.delete(cacheName)
+      })
+    )
+  })
+})
+
+describe('Home page functionality', () => {
+  it('Loads the welcome card', () => {
+    cy.visit('/', {
+      onBeforeLoad: function (win) {
+        const promise = new Promise(function (resolve) {})
+        // eslint-disable-next-line no-return-assign
+        return win.navigator.serviceWorker.register = function () {
+          return promise
+        }
+      }
+    })
+    cy.contains('h1', 'Welcome to Pelilauta')
+  })
+  it('Loads the Mekanismi card', () => {
+    cy.visit('/')
+    cy.contains('h1', 'Mekanismi recent changes')
+  })
+})
