4272f36787ce8cd5b14ba02205ffb4e001af986e
Ville Takanen <vitkukissa@gmail.com>
2021-03-13 15:15:48 +0200

    feat: e2e test user login added

--- src/views/admin/TestLoginView.vue
+++ src/views/admin/TestLoginView.vue
@@ -2,12 +2,42 @@
   <p>This view exists solely to enable e2e testing suite accounts to login.</p><p /><div>
     <input
       id="usernameField"
+      v-model="username"
       type="textfield"
     >
     <input
       id="passwordField"
+      v-model="password"
       type="textfield"
     >
-    <button id="post" />
+    <button
+      id="post"
+      @click="login"
+    />
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import { useRouter } from 'vue-router'
+
+export default defineComponent({
+  name: 'TestLoginView',
+  setup () {
+    const password = ref('')
+    const username = ref('')
+    const router = useRouter()
+    const login = () => {
+      firebase.auth().signInWithEmailAndPassword(username.value, password.value).then(() => {
+        router.push('/profile')
+      }).catch((error) => {
+        console.debug(error)
+        router.push('/login')
+      })
+    }
+    return { username, password, login }
+  }
+})
+</script>
