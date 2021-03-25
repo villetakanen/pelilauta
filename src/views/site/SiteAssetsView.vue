8424cdb743458534af320558c8c9390235ac6f97
Ville Takanen <vitkukissa@gmail.com>
2021-03-25 11:20:00 +0200

    feat: add asset button

--- src/views/site/SiteAssetsView.vue
+++ src/views/site/SiteAssetsView.vue
@@ -1,50 +1,60 @@
 <template>
-  <div class="siteAssetsView viewFull">
-    <SiteToolbar />
-    <transition name="fade">
-      <div
-        class="suspense mekanismiGrid"
-      >
-        <MaterialCard class="mainCard">
-          <div v-if="site && site.id">
-            <div
-              class="toolbar"
-              style="margin-bottom: 16px"
-            >
-              <h1>{{ $t('mekanismi.attachments.title') }}</h1>
-              <div class="spacer" />
-              <AttachFileButton />
-            </div>
-            <div class="attachments">
-              <AttachmentRow
-                v-for="asset, key in assets"
-                :key="key"
-                :asset="asset[1]"
-              />
-            </div>
+  <div class="siteAssetsView">
+    <div style="margin: 0 8px">
+      <SiteToolbar />
+    </div>
+    <div class="mekanismiGrid">
+      <div class="mainCard">
+        <div v-if="site && site.id">
+          <h3>{{ $t('mekanismi.attachments.title') }}</h3>
+          <div class="attachments">
+            <AttachmentRow
+              v-for="asset, key in assets"
+              :key="key"
+              :asset="asset[1]"
+            />
           </div>
-          <Loader
-            v-else
-            poster
-          />
-        </MaterialCard>
-        <MaterialCard class="sideCard">
-          <SideBar />
-        </MaterialCard>
+        </div>
+        <Loader
+          v-else
+          poster
+        />
       </div>
-    </transition>
+      <Card
+        class="sideCard"
+        :rise="4"
+      >
+        <SideBar />
+      </Card>
+    </div>
+    <teleport to="#ScreenBottomFloatRight">
+      <Fab
+        v-if="crudActions"
+        id="addAssetFab"
+        to="/site/add/asset"
+        :text="$t('action.add')"
+        color="tertiary"
+      >
+        <Icon
+          dark
+          name="addAnImage"
+        />
+      </Fab>
+    </teleport>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { computed, defineComponent } from 'vue'
 import { useSite, useAssets } from '@/state/site'
-import MaterialCard from '@/components/material/MaterialCard.vue'
 import Loader from '@/components/app/Loader.vue'
 import SideBar from '@/components/wikipage/SideBar.vue'
-import AttachFileButton from '@/components/sites/AttachFileButton.vue'
 import AttachmentRow from '@/components/site/assets/AssetListItem.vue'
 import SiteToolbar from '@/components/sites/SiteToolbar.vue'
+import Card from '@/components/layout/Card.vue'
+import Fab from '@/components/material/Fab.vue'
+import { useAuthState } from '@/state/authz'
+import Icon from '@/components/material/Icon.vue'
 
 /**
  * A Router view for a asset management screen of a site
@@ -57,12 +67,13 @@ import SiteToolbar from '@/components/sites/SiteToolbar.vue'
 export default defineComponent({
   name: 'SiteAssetsView',
   components: {
-    MaterialCard,
     Loader,
     SideBar,
-    AttachFileButton,
     AttachmentRow,
-    SiteToolbar
+    SiteToolbar,
+    Card,
+    Fab,
+    Icon
   },
   props: {
     siteid: {
@@ -71,9 +82,15 @@ export default defineComponent({
     }
   },
   setup (props) {
-    const { site } = useSite(props.siteid)
+    const { site, hasAdmin } = useSite(props.siteid)
+    const { uid } = useAuthState()
     const { assets } = useAssets()
-    return { site, assets }
+
+    const crudActions = computed(() => {
+      return hasAdmin(uid.value)
+    })
+
+    return { site, assets, crudActions }
   }
 })
 </script>
