971efdf13ebff16f23b3dc781e5402406b800bca
Ville Takanen <vitkukissa@gmail.com>
2021-03-25 10:53:59 +0200

    feat: adding an asset updates state and db

--- src/state/site/assets.ts
+++ src/state/site/assets.ts
@@ -24,6 +24,54 @@ export interface Asset {
 const siteAssets = ref(new Map<string, Asset>())
 const assets = computed(() => siteAssets.value)
 
+async function patchAsset (storageAsset: firebase.storage.Reference): Promise<void> {
+  const database = firebase.firestore()
+  const firestoreAssetsRef = database.collection('sites').doc(siteid).collection('assetMeta')
+
+  const url = await storageAsset.getDownloadURL()
+  const databaseAssetRef = firestoreAssetsRef.doc(storageAsset.name)
+  let databaseAssetDoc = await databaseAssetRef.get()
+
+  // migration code for db sanity, might want to live in pelilauta-functions, or be deprecated at some point
+  try {
+    if (!(await databaseAssetDoc).exists) {
+      console.debug('migtrating asset', siteid, storageAsset.name)
+      await databaseAssetRef.set({
+        lastUpdate: firebase.firestore.FieldValue.serverTimestamp(),
+        creator: 'migrated by the app'
+      })
+      databaseAssetDoc = await databaseAssetRef.get()
+    }
+  } catch (error) {
+    console.error('insufficcient priviledges to update db', error)
+  }
+
+  siteAssets.value.set(storageAsset.name, {
+    name: storageAsset.name,
+    url: url,
+    creator: databaseAssetDoc.data()?.creator || '',
+    lastUpdate: databaseAssetDoc.data()?.lastUpdate || null
+  })
+}
+
+export async function upload (file:File, uid: string): Promise<void> {
+  const storageRef = firebase.storage().ref()
+  const fileRef = storageRef.child(siteid + '/' + file.name)
+  const snapshot = fileRef.put(file)
+
+  const storageAsset = (await snapshot).ref
+
+  const database = firebase.firestore()
+  const firestoreAssetRef = database.collection('sites').doc(siteid).collection('assetMeta').doc(file.name)
+
+  await firestoreAssetRef.set({
+    creator: uid,
+    lastUpdate: firebase.firestore.FieldValue.serverTimestamp()
+  })
+
+  return patchAsset(storageAsset)
+}
+
 export async function subscribeTo (id: string): Promise<void> {
   if (siteid === id) return
   siteid = id
@@ -31,37 +79,8 @@ export async function subscribeTo (id: string): Promise<void> {
   const storage = firebase.storage()
   const storageAssetList = await storage.ref().child(siteid).listAll()
 
-  const database = firebase.firestore()
-  const firestoreAssetsRef = database.collection('sites').doc(siteid).collection('assetMeta')
-
   siteAssets.value = new Map<string, Asset>()
-
-  storageAssetList.items.forEach(async (storageAsset) => {
-    const url = await storageAsset.getDownloadURL()
-    const databaseAssetRef = firestoreAssetsRef.doc(storageAsset.name)
-    let databaseAssetDoc = await databaseAssetRef.get()
-
-    // migration code for db sanity, might want to live in pelilauta-functions, or be deprecated at some point
-    try {
-      if (!(await databaseAssetDoc).exists) {
-        console.debug('migtrating asset', siteid, storageAsset.name)
-        await databaseAssetRef.set({
-          lastUpdate: firebase.firestore.FieldValue.serverTimestamp(),
-          creator: 'migrated by the app'
-        })
-        databaseAssetDoc = await databaseAssetRef.get()
-      }
-    } catch (error) {
-      console.error('insufficcient priviledges to update db', error)
-    }
-
-    siteAssets.value.set(storageAsset.name, {
-      name: storageAsset.name,
-      url: url,
-      creator: databaseAssetDoc.data()?.creator || '',
-      lastUpdate: databaseAssetDoc.data()?.lastUpdate || null
-    })
-  })
+  storageAssetList.items.forEach(async (storageAsset) => patchAsset(storageAsset))
 }
 
 export function useAssets (): { assets: ComputedRef<Map<string, Asset>> } {
