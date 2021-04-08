b016cfd8a1b1dac321f10ea924c31380bbfb1ca6
Ville Takanen <vitkukissa@gmail.com>
2021-04-08 14:03:43 +0300

    fix: refactored profile field editor ui

--- src/components/profile/PublicProfile.vue
+++ src/components/profile/PublicProfile.vue
@@ -1,61 +1,52 @@
 <template>
-  <transition name="fade">
-    <MaterialCard
-      v-if="profile"
-    >
-      <h2>{{ $t('profile.publicInfoTitle') }}</h2>
-      <p>{{ $t('profile.publicInfoHelper') }}</p>
-      <div class="materialField">
-        <label for="nickName">Nick</label>
-        <input
-          id="nickName"
+  <div class="publicProfile">
+    <h3>{{ $t('profile.publicInfoTitle') }}</h3>
+    <p>{{ $t('profile.publicInfoHelper') }}</p>
+    <div class="profileForm">
+      <div class="avatarTool">
+        ...
+      </div>
+      <div class="profileFields">
+        <TextField
           v-model="v.nickname.$model"
-          class="material-textfield"
-          :class="{ error: v.nickname.$error }"
-        >
-        <MaterialButton
-          icon
-          inline
-          :disabled="v.nickname.$error || !v.nickname.$dirty"
-          :action="saveNick"
-        >
-          <img src="@/assets/icons/save.svg">
-        </MaterialButton>
-      </div>
-      <div class="materialField">
-        <label for="tagline">{{ $t('profile.tagline') }}</label>
-        <input
-          id="tagline"
+          :label="$t('profile.nick')"
+          :error="v.nickname.$error"
+        />
+        <TextField
           v-model="v.tagline.$model"
-          class="material-textfield"
-        >
-        <MaterialButton
-          icon
-          inline
-          :disabled="v.tagline.$error || !v.tagline.$dirty"
-          :action="saveTagline"
-        >
-          <img src="@/assets/icons/save.svg">
-        </MaterialButton>
+          :label="$t('profile.tagline')"
+          :error="v.tagline.$error"
+        />
+        <div style="text-align: right">
+          <MaterialButton
+            :async-action="save"
+            text
+            :disabled="v.$errors.length > 0 || !v.$dirty"
+          >
+            {{ $t('action.save') }}
+          </MaterialButton>
+        </div>
       </div>
-    </MaterialCard>
-  </transition>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, computed, ref, Ref } from 'vue'
 import { useVuelidate } from '@vuelidate/core'
 import { required } from '@vuelidate/validators'
-import MaterialCard from '@/components/material/MaterialCard.vue'
 import MaterialButton from '@/components/material/MaterialButton.vue'
 import { useProfile } from '@/state/authz'
 import { useAuthors } from '@/state/authors'
+import TextField from '../material/TextField.vue'
+import { useSnack } from '@/composables/useSnack'
+import { useI18n } from 'vue-i18n'
 
 export default defineComponent({
   name: 'PublicProfileInfoCard',
   components: {
-    MaterialCard,
-    MaterialButton
+    MaterialButton,
+    TextField
   },
   setup () {
     const { profile, updateProfile } = useProfile()
@@ -68,10 +59,6 @@ export default defineComponent({
         localNick.value = val
       }
     })
-    const saveNick = () => {
-      if (!v.value.nickname.$error &&
-        typeof localNick.value === 'string') updateProfile({ nick: localNick.value })
-    }
 
     // Tagline field and persistency
     const localTagline:Ref<string|null> = ref(null)
@@ -81,10 +68,6 @@ export default defineComponent({
         localTagline.value = val || ''
       }
     })
-    const saveTagline = () => {
-      if (!v.value.tagline.$error &&
-        typeof localTagline.value === 'string') updateProfile({ tagline: localTagline.value })
-    }
 
     const { authors } = useAuthors()
     const uniqueNick = (value:any) => (value.toLowerCase() === profile.value.nick?.toLowerCase() || !authors.value.find((target) => (target.nick?.toLowerCase() === value.toLowerCase())))
@@ -96,33 +79,29 @@ export default defineComponent({
       tagline: { maxLength, minLength }
     }
     const v = useVuelidate(rules, { nickname, tagline })
+    const { pushSnack } = useSnack()
+    const i18n = useI18n()
+    const save = async () => {
+      await updateProfile({ nick: '' + localNick.value, tagline: '' + localTagline.value })
+      pushSnack(i18n.t('snacks.updateSuccess'))
+    }
 
-    return { profile, nickname, tagline, v, saveNick, saveTagline }
+    return { profile, nickname, tagline, v, save }
   }
 })
 </script>
 
 <style lang="sass" scoped>
 @import @/styles/include-media.scss
-@import @/styles/material-colors.sass
 @import @/styles/material-typography.sass
 
-.materialField
-  height: 56px
-  box-sizing: border-box
-  padding: 8px 0
-  display: block
-  position: relative
-  // border: solid 1px red
-  label
-    position: absolute
-    top: 0px
-    left: 4px
-    @include TypeCaption()
-  label+input
-    margin-top: 10px
-.material-textfield
-  margin-right: 8px
-  max-width: 300px
+.publicProfile
+  .profileForm
+    display: flex
+    .avatarTool
+      height: 72px
+      width: 72px
+    .profileFields
+      flex-grow: 1
 
 </style>
