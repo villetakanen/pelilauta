2d4641a177716207c173833ba524f54b5a118951
Ville Takanen <vitkukissa@gmail.com>
2021-04-11 13:59:19 +0300

    feat: add page redirects to route, not dialog

---
+++ src/views/site/AddPageView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="addPageView">
+    <SiteToolbar />
+    <div class="">
+      <h3>{{ $t('site.actions.addPage') }}</h3>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useSite } from '@/state/site'
+import SiteToolbar from '@/components/sites/SiteToolbar.vue'
+
+export default defineComponent({
+  name: 'AddPageView',
+  components: {
+    SiteToolbar
+  },
+  props: {
+    siteid: {
+      type: String,
+      required: true
+    }
+  },
+  setup (props) {
+    const { site } = useSite(props.siteid)
+
+    return { site }
+  }
+})
+</script>
