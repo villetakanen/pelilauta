69b4a9b5a0e9b269bd9b28f84ff1962eac8844cb
Ville Takanen <vitkukissa@gmail.com>
2021-05-18 11:34:35 +0300

    feat: sitecategories card added to meta

---
+++ src/components/site/SiteCategoriesCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <Card class="siteCategoriesCard">
+    siteCategoriesCard
+  </Card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Card from '../layout/Card.vue'
+
+export default defineComponent({
+  components: { Card }
+})
+</script>
