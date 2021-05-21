de749e2730c339e2daa1a5de43a8ae303fbceb91
Ville Takanen <vitkukissa@gmail.com>
2021-05-21 14:53:56 +0300

    fix: pinned topic zipped for mobile

---
+++ src/components/topic/PinnedThreadCard.vue
@@ -0,0 +1,94 @@
+<template>
+  <Card
+    class="pinnedThreadCard"
+    secondary
+    :rise="0"
+  >
+    <h1 class="cardTitle">
+      <router-link :to="`/thread/${thread.id}/view/`">
+        {{ thread.data.title }}
+      </router-link>
+    </h1>
+    <p
+      class="contentSnippet onlyForDesktop"
+    >
+      {{ snippet }}
+    </p>
+    <transition name="fade">
+      <div>
+        <Pill
+          v-if="newReplies"
+          small
+          prepend-icon="send"
+          dark
+        >
+          <router-link :to="`/thread/${thread.id}/view/from/${seen}`">
+            {{ thread.replyCount ? thread.replyCount + ' ' + $t('post.nOfReplies') : $t('post.more') }}
+          </router-link>
+        </Pill>
+        <p
+          v-else
+          class="topic"
+        >
+          <router-link :to="`/thread/${thread.id}/view/from/${seen}`">
+            {{ thread ? thread.replyCount + ' ' + $t('post.nOfReplies') : $t('post.more') }}
+          </router-link>
+        </p>
+      </div>
+    </transition>
+  </Card>
+</template>
+
+<script lang="ts">
+import { Thread } from '@/utils/firestoreInterfaces'
+import { computed, defineComponent, PropType } from 'vue'
+import Card from '../layout/Card.vue'
+
+export default defineComponent({
+  components: { Card },
+  props: {
+    thread: {
+      type: Object as PropType<Thread>,
+      required: true
+    }
+  },
+  setup (props) {
+    const snippet = computed(() => {
+      const div = document.createElement('div')
+      div.innerHTML = props.thread.data.content
+      let snip = ''
+      if (div.firstChild) {
+        snip = div.firstChild.textContent || ''
+        if (snip.length > 172) snip = snip.substring(0, 172) + '...'
+      }
+      return snip
+    })
+    return { snippet }
+  }
+})
+</script>
+
+<style lang="sass" scoped>
+@import @/styles/include-media.scss
+@import @/styles/material-typography.sass
+
+.pinnedThreadCard
+  h1.cardTitle
+    @include TypeHeadline6()
+    padding-top: 0
+    margin-top: 0
+    line-height: 24px
+    a
+      color: var(--chroma-secondary-i)
+  .contentSnippet
+    @include TypeBody2()
+    margin-top: 8px
+    margin-bottom: 8px
+    color: var(--chroma-secondary-h)
+  .topic
+    text-align: right
+    @include TypeBody2()
+    a
+      color: var(--chroma-secondary-b)
+      text-decoration: none
+</style>
