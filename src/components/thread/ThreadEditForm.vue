38573d03213cd671017f2b4186530fba9027e065
Ville Takanen <vitkukissa@gmail.com>
2021-06-08 13:44:20 +0300

    fix: code quality fixes

--- src/components/thread/ThreadEditForm.vue
+++ src/components/thread/ThreadEditForm.vue
@@ -7,6 +7,12 @@
         :label="$t('threads.title')"
         :error="v.threadTitle.$error"
       />
+      <MaterialSelect
+        v-model="v.threadTopic.$model"
+        class="field"
+        :opts="topicOpts"
+        :label="$t('threads.topic')"
+      />
     </div>
     <DocumentEditor v-if="!threadid || thread.data.content" />
   </div>
@@ -15,16 +21,18 @@
 <script lang="ts">
 import { Site } from '@/state/site'
 import useVuelidate from '@vuelidate/core'
-import { defineComponent, onMounted, PropType, ref } from 'vue'
+import { defineComponent, PropType, ref } from 'vue'
 import { required } from '@vuelidate/validators'
 import DocumentEditor from '../editor/DocumentEditor.vue'
 import TextField from '../material/TextField.vue'
+import MaterialSelect from '../material/MaterialSelect.vue'
 
 export default defineComponent({
   name: 'ThreadEditForm',
   components: {
     DocumentEditor,
-    TextField
+    TextField,
+    MaterialSelect
   },
   props: {
     threadid: {
@@ -41,10 +49,12 @@ export default defineComponent({
   setup () {
     // Thread name
     const threadTitle = ref('')
+    const threadTopic = ref('')
 
     const v = useVuelidate({
-      threadTitle: { required }
-    }, { threadTitle })
+      threadTitle: { required },
+      threadTopic: { required }
+    }, { threadTitle, threadTopic })
 
     return { v }
   }
