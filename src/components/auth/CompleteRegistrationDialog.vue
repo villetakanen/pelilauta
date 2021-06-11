227386cfd949fc9fc06e470600caea174d0c28d5
Ville Takanen <vitkukissa@gmail.com>
2021-06-11 13:04:05 +0300

    fix: translations, app structure polish

--- src/components/auth/CompleteRegistrationDialog.vue
+++ src/components/auth/CompleteRegistrationDialog.vue
@@ -1,18 +1,21 @@
 <template>
   <Dialog v-model="registrationIncomplete">
-    <h1 class="title">
-      {{ $t('registration.title') }}
-    </h1>
+    <Card>
+      <h1 class="title">
+        {{ $t('registration.title') }}
+      </h1>
+    </Card>
   </Dialog>
 </template>
 
 <script lang="ts">
 import { useAuth } from '@/state/authz'
 import { defineComponent } from 'vue'
+import Card from '../layout/Card.vue'
 import Dialog from '../material/Dialog.vue'
 
 export default defineComponent({
-  components: { Dialog },
+  components: { Dialog, Card },
   setup () {
     const { registrationIncomplete } = useAuth()
     return { registrationIncomplete }
