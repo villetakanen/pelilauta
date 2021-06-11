daa9e8886bd2d3dd28659f0714fae70e09ee8f1f
Ville Takanen <vitkukissa@gmail.com>
2021-06-11 12:56:28 +0300

    feat: restored the registration dialog
    profile must be stored, and the rights waiver red at the registration

---
+++ src/components/auth/CompleteRegistrationDialog.vue
@@ -0,0 +1,21 @@
+<template>
+  <Dialog v-model="registrationIncomplete">
+    <h1 class="title">
+      {{ $t('registration.title') }}
+    </h1>
+  </Dialog>
+</template>
+
+<script lang="ts">
+import { useAuth } from '@/state/authz'
+import { defineComponent } from 'vue'
+import Dialog from '../material/Dialog.vue'
+
+export default defineComponent({
+  components: { Dialog },
+  setup () {
+    const { registrationIncomplete } = useAuth()
+    return { registrationIncomplete }
+  }
+})
+</script>
--- src/state/authz/index.ts
+++ src/state/authz/index.ts
@@ -4,25 +4,57 @@ import 'firebase/analytics'
 import { useProfile, PublicProfile, ProfileMeta } from './profile'
 import { useAuthState } from './state'
 import { useAssets } from './assets'
-import { reactive } from 'vue'
+import { computed, ComputedRef, reactive } from 'vue'
 
 const authState = reactive({
-  session: false,
+  missingProfileData: false,
   anonymous: true,
+  admin: false,
   user: {
     uid: ''
   }
 })
+const user = computed(() => (authState.user))
+const registrationIncomplete = computed(() => (authState.missingProfileData))
+
+let unsubscribeProfile:CallableFunction|undefined
+
+function fetchProfile () {
+  if (unsubscribeProfile) unsubscribeProfile()
+
+  const db = firebase.firestore()
+  const profileRef = db.collection('profiles').doc(authState.user.uid)
+  unsubscribeProfile = profileRef.onSnapshot((snap) => {
+    if (snap.exists) {
+      // ...
+      if (!snap.data()?.nick) {
+        console.debug('profile is missing a nickname: please display the registration dialog')
+        authState.missingProfileData = true
+      }
+    } else {
+      console.debug('user does not have a profile entry: lets create that through the registration dialog')
+      authState.missingProfileData = true
+    }
+  })
+}
 
 function onAuthStateChanged (user: firebase.User|null) {
   if (!user || user.isAnonymous) {
     console.debug('state.authz.onAuthStateChanged', 'anonymous')
+    authState.missingProfileData = false
     authState.anonymous = true
+    authState.admin = false
     authState.user = {
       uid: ''
     }
   } else {
     console.debug('state.authz.onAuthStateChanged', user.displayName, user.uid)
+    authState.anonymous = false
+    authState.admin = false
+    authState.user = {
+      uid: user.uid
+    }
+    fetchProfile()
   }
 }
 
@@ -35,10 +67,15 @@ function createAuth (): void {
   })
 }
 
+function useAuth (): { user: ComputedRef<{ uid: string }>, registrationIncomplete: ComputedRef<boolean> } {
+  return { user, registrationIncomplete }
+}
+
 export {
   ProfileMeta,
   PublicProfile,
   createAuth,
+  useAuth,
   useAuthState,
   useAssets,
   useProfile
