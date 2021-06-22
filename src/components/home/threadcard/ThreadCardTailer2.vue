299ece76ee32999c3abca643609e8cfd9110acb5
Ville Takanen <vitkukissa@gmail.com>
2021-06-22 11:20:01 +0300

    fix: love a thread on home card moved to comp

--- src/components/home/threadcard/ThreadCardTailer2.vue
+++ src/components/home/threadcard/ThreadCardTailer2.vue
@@ -5,8 +5,14 @@
       withSite: thread.site
     }"
   >
-    <div style="min-width: 64px">
-      ...
+    <div style="min-width: 64px; display: block">
+      <div style="display: inline-block">
+        <LoveAThreadAction
+          :loves="loves"
+          :count="thread.lovedCount"
+          :action="toggleLove"
+        />
+      </div>
     </div>
     <div class="topicLink spacer">
       {{ $t('stream.inStream') }} <router-link :to="`/stream/topic/${thread.data.topic}`">
@@ -18,13 +24,16 @@
 </template>
 
 <script lang="ts">
+import LoveAThreadAction from '@/components/thread/LoveAThreadAction.vue'
+import { useAuth, useProfile } from '@/state/authz'
 import { useMeta } from '@/state/meta'
 import { Thread } from '@/utils/firestoreInterfaces'
+import { loveThread, unloveThread } from '@/state/threads'
 import { computed, defineComponent, PropType } from 'vue'
 import RepliesCardToolbarLink from './RepliesCardToolbarLink.vue'
 
 export default defineComponent({
-  components: { RepliesCardToolbarLink },
+  components: { RepliesCardToolbarLink, LoveAThreadAction },
   props: {
     thread: {
       type: Object as PropType<Thread>,
@@ -36,7 +45,20 @@ export default defineComponent({
     const topicName = computed(() => {
       return streams.value.find((val) => (val.slug === props.thread.data.topic))?.name
     })
-    return { topicName }
+    const { profileMeta } = useProfile()
+    const loves = computed(() => (profileMeta.value.lovedThreads?.includes(props.thread.id)))
+
+    const { user } = useAuth()
+    async function toggleLove () {
+      if (props.thread.author === user.value.uid) {
+        console.warn('trying to love a thread the user is an author of')
+        return
+      }
+      if (loves.value) return unloveThread(user.value.uid, props.thread.id)
+      return loveThread(user.value.uid, props.thread.id)
+    }
+
+    return { topicName, loves, toggleLove }
   }
 })
 </script>
