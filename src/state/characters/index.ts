90948d3a75ef70a79590a8bc7045dc0181ff3c94
Ville Takanen <vitkukissa@gmail.com>
2021-08-02 20:20:28 +0300

    feat: adding characters supported by state

--- src/state/characters/index.ts
+++ src/state/characters/index.ts
@@ -1,14 +1,26 @@
-import { ref } from 'vue'
+import { ref, Ref } from 'vue'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/analytics'
 import { PlayerCharacter } from '@/utils/firestoreInterfaces'
 import { useSite } from '../site'
 
 const characters = ref(new Map<string, PlayerCharacter>())
 
-function addPlayerCharacter (type: string) {
+async function addPlayerCharacter (type: string) {
   const { site } = useSite()
   console.debug('adding a character to', site.value.id, site.value.name)
+  if (site.value.usePlayers) {
+    const db = firebase.firestore()
+    return (await db.collection('sites').doc(site.value.id).collection('characters').add({ type: type })).id
+  } else {
+    throw new Error('can not add characters to a game with no player functions')
+  }
 }
 
-export function useCharacters () {
-  return { characters }
+export function useCharacters (): {
+    addPlayerCharacter: (type: string) => Promise<string>
+    characters: Ref<Map<string, PlayerCharacter>>
+    } {
+  return { addPlayerCharacter, characters }
 }
