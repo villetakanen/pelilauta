dc99c1aa9ffa618a5f420c1506f7a6f3f411c39d
Ville Takanen <vitkukissa@gmail.com>
2021-08-02 11:42:00 +0300

    feat: character mgmnt card

---
+++ src/components/site/characters/SiteCharactersCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <Card class="charactersCard">
+    <h1 class="title">
+      {{ $t('site.characters.title') }}
+    </h1>
+    <div>
+      <MaterialSelect :label="$t('site.characters.template')" />
+      <MaterialButton />
+    </div>
+  </Card>
+</template>
+
+<script lang="ts">
+import Card from '@/components/layout/Card.vue'
+import MaterialButton from '@/components/material/MaterialButton.vue'
+import MaterialSelect from '@/components/material/MaterialSelect.vue'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  components: { Card, MaterialSelect, MaterialButton }
+})
+</script>
+
+<style lang="sass" scoped>
+</style>
