f15a508195152c6ad4c912677e4f0067f7006754
Ville Takanen <vitkukissa@gmail.com>
2021-08-03 09:52:49 +0300

    feat: adding and listing characters works

--- src/components/site/characters/SiteCharactersCard.vue
+++ src/components/site/characters/SiteCharactersCard.vue
@@ -3,13 +3,21 @@
     <h1 class="title">
       {{ $t('site.characters.title') }}
     </h1>
+    <div
+      v-for="pc in pcs"
+      :key="pc[0]"
+    >
+      {{ pc[1].name }}
+    </div>
     <div>
       <MaterialSelect
         :label="$t('site.characters.template')"
         :opts="templates"
-        model-value="default"
+        :model-value="newCharType"
       />
-      <MaterialButton>{{ $t('action.add') }}</MaterialButton>
+      <MaterialButton :async-action="addCharacter">
+        {{ $t('action.add') }}
+      </MaterialButton>
     </div>
   </Card>
 </template>
@@ -18,18 +26,29 @@
 import Card from '@/components/layout/Card.vue'
 import MaterialButton from '@/components/material/MaterialButton.vue'
 import MaterialSelect from '@/components/material/MaterialSelect.vue'
-import { defineComponent } from 'vue'
+import { useCharacters } from '@/state/characters'
+import { defineComponent, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 
 export default defineComponent({
   components: { Card, MaterialSelect, MaterialButton },
   setup () {
     const i18n = useI18n()
+    const newCharType = ref('default')
     const templates = [
       { key: 'default', value: i18n.t('site.characters.templates.default') },
       { key: 'll', value: i18n.t('site.characters.templates.ll') }
     ]
-    return { templates }
+    const { characters: pcs, addPlayerCharacter } = useCharacters()
+    async function addCharacter () {
+      try {
+        const newCharID = await addPlayerCharacter(newCharType.value)
+        console.debug('added player character', newCharID)
+      } catch (e) {
+        console.error(e)
+      }
+    }
+    return { templates, pcs, newCharType, addCharacter }
   }
 })
 </script>
