8aa7c5c0caed24652b42278569a4c0f2f1a24bdd
Ville Takanen <vitkukissa@gmail.com>
2021-09-13 14:51:04 +0300

    fix: name field saves on blur

--- src/components/character/CharacterMetaForm.vue
+++ src/components/character/CharacterMetaForm.vue
@@ -1,13 +1,18 @@
 <template>
   <Card>
-    <h1 class="title">
-      {{ $t('character.meta.title') }}
-    </h1>
-    <TextField :label="$t('character.stats.name')" />
+    <Icon
+      :name="site.systemBadge+'-logo'"
+      headline
+    />
     <MaterialSelect
-      v-model="site"
+      v-model="siteid"
       :opts="siteOpts"
       :label="$t('character.stats.game')"
+    />
+    <TextField
+      v-model="charname"
+      :label="$t('character.stats.name')"
+      blur
     />
   </Card>
 </template>
@@ -20,24 +25,35 @@ import { useSites } from '@/state/sites'
 import { useCharacters } from '@/state/characters'
 import TextField from '../material/TextField.vue'
 import { useSite } from '@/state/site'
+import Icon from '../material/Icon.vue'
 
 export default defineComponent({
   name: 'CharacterMetaForm',
-  components: { MaterialSelect, Card, TextField },
+  components: { MaterialSelect, Card, TextField, Icon },
   setup () {
     const { characterid, character, updatePlayerCharacterFields } = useCharacters()
     const { userSites } = useSites()
+    const { site } = useSite()
     const siteOpts = computed(() => {
       return userSites.value.filter((s) => (s.usePlayers)).map((s) => ({ key: s.id, value: s.name }))
     })
-    const site = computed({
+    const siteid = computed({
       get: () => (character?.value?.siteid ?? ''),
       set: (s: string) => {
         updatePlayerCharacterFields(characterid.value, { siteid: s })
         useSite(s)
       }
     })
-    return { siteOpts, site }
+    const charname = computed({
+      get: () => (character?.value?.name ?? ''),
+      set: (n: string) => {
+        console.debug('name', n)
+        if (n !== character?.value?.name) {
+          updatePlayerCharacterFields(characterid.value, { name: n })
+        }
+      }
+    })
+    return { siteOpts, siteid, site, charname }
   }
 })
 </script>
