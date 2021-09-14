e682e08bab946bf5f8862afb6817202b27baead4
Ville Takanen <vitkukissa@gmail.com>
2021-09-14 10:48:41 +0300

    feat: editablecolumn added

--- src/views/characters/CharacterView.vue
+++ src/views/characters/CharacterView.vue
@@ -8,9 +8,21 @@
         {{ character.name }}
       </h1>
       <div class="flexLayout">
-        <CharacterMetaForm class="flex-span-1" />
-        <RichTextEditor class="flex-span-2" />
-
+        <CharacterMetaForm
+          class="flex-span-1"
+          style="border-bottom: solid 1px var(--color-b-h)"
+        />
+        <EditableColumn
+          class="flex-span-2"
+          style="border-bottom: solid 1px var(--color-b-h)"
+        >
+          <template #viewer>
+            <div :innerHTML="character.htmlContent" />
+          </template>
+          <template #editor>
+            <RichTextEditor />
+          </template>
+        </EditableColumn>
         <div class="debug flex-span-2">
           {{ character }}
         </div>
@@ -26,10 +38,11 @@ import RichTextEditor from '@/components/quill/RichTextEditor.vue'
 import { useSite } from '@/state/site'
 import { PlayerCharacter } from '@/utils/firestoreInterfaces'
 import CharacterMetaForm from '@/components/character/CharacterMetaForm.vue'
+import EditableColumn from '@/components/layout/EditableColumn.vue'
 
 export default defineComponent({
   name: 'CharacterView',
-  components: { RichTextEditor, CharacterMetaForm },
+  components: { RichTextEditor, CharacterMetaForm, EditableColumn },
   props: {
     id: {
       type: String,
