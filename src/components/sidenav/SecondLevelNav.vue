3cc74ed6a1859750685e9272dafc35e261bbe474
Ville Takanen <vitkukissa@gmail.com>
2021-09-28 08:46:08 +0300

    fix: snack no longer clips for menu (on desktop)

--- src/components/sidenav/SecondLevelNav.vue
+++ src/components/sidenav/SecondLevelNav.vue
@@ -1,5 +1,16 @@
 <template>
-  <nav class="secondLevelNav">
+  <nav id="secondLevelNav">
     <slot />
   </nav>
 </template>
+
+<style lang="sass" scoped>
+@import @/styles/include-media.scss
+
+@include media('>=tablet')
+  nav#secondLevelNav
+    position: absolute
+    top: 0
+    left: 0
+    width: 310px
+</style>
