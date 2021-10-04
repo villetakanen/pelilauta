fbea812466c3f367163091d74c6538d9ac4387ed
Ville Takanen <vitkukissa@gmail.com>
2021-10-04 10:26:20 +0300

    fix: backported stylebook buttons view

--- src/components/stylebook/FormControls.vue
+++ src/components/stylebook/FormControls.vue
@@ -1,6 +1,10 @@
 <template>
   <Column class="formContols">
     <h1>Forms</h1>
+    <p>
+      And other input components.
+    </p>
+    <h2>Input fields</h2>
     <Toggle
       v-model="error"
       label="Set fields to error state"
@@ -29,6 +33,39 @@
       />
       <p>value: <span class="code">{{ textFieldValue }}</span></p>
     </div>
+
+    <h2>Buttons</h2>
+    <Button @action="alert">
+      An Alert Button
+    </Button>
+    <Button
+      disabled
+      @action="alert"
+    >
+      A Disabled Alert
+    </Button>
+    <Button
+      secondary
+      :working="working"
+      @click="working=!working"
+    >
+      Secondary + Async
+    </Button>
+    <Button
+      secondary
+      disabled
+    >
+      Secondary + Disabled
+    </Button>
+    <Button tertiary>
+      Tertiary
+    </Button>
+    <Button
+      tertiary
+      disabled
+    >
+      Tertiary + Disabled
+    </Button>
   </Column>
 </template>
 
@@ -38,21 +75,22 @@ import Select from '../form/Select.vue'
 import Column from '../layout/Column.vue'
 import Textfield from '../form/Textfield.vue'
 import Toggle from '../material/Toggle.vue'
-
+import Button from '../form/Button.vue'
 export default defineComponent({
   name: 'FromControls',
-  components: { Column, Select, Toggle, Textfield },
+  components: { Column, Select, Toggle, Textfield, Button },
   setup () {
     const error = ref(false)
-
     const selectOps = new Map<string, string>()
     const selectKey = ref('one')
     selectOps.set('one', 'Option one')
     selectOps.set('two', 'Option two')
-
     const textFieldValue = ref('')
-
-    return { selectKey, selectOps, error, textFieldValue }
+    function alert () {
+      window.alert('An annoying test functionality')
+    }
+    const working = ref(false)
+    return { selectKey, selectOps, error, textFieldValue, alert, working }
   }
 })
 </script>
