d64a695a83494c238ee9a8d08878de33d85ae5a9
Ville Takanen <vitkukissa@gmail.com>
2021-10-11 22:04:15 +0300

    fix: view title split to a component

---
+++ src/components/layout/ViewTitle.vue
@@ -0,0 +1,49 @@
+<template>
+  <div
+    class="viewTitle"
+    :class="{ withIcon: icon && icon.length > 0 }"
+  >
+    <Icon
+      v-if="icon"
+      :name="icon"
+    />
+    <h2 v-if="parent">
+      <router-link
+        v-if="parentRoute"
+        :to="parentRoute"
+      >
+        {{ parent }}
+      </router-link>
+      <template v-else>
+        {{ parent }}
+      </template>
+    </h2>
+    <h1><slot /></h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Icon from '../material/Icon.vue'
+
+export default defineComponent({
+  components: { Icon },
+  props: {
+    icon: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    parent: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    parentRoute: {
+      type: String,
+      required: false,
+      default: ''
+    }
+  }
+})
+</script>
