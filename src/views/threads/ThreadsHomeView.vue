65d74f2c479bbd3700a5cb7b2a8a4619342f5078
Ville Takanen <vitkukissa@gmail.com>
2021-10-20 08:53:19 +0300

    feat: added a new discussions home page

---
+++ src/views/threads/ThreadsHomeView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="ThreadsHomeView">
+    <Header>
+      <ViewTitle icon="discussion">
+        {{ $t('threads.home.title') }}
+      </ViewTitle>
+    </Header>
+    <main class="bookLayout">
+      <section class="double-cut">
+        -- 5 newest threads --
+      </section>
+      <section class="double-cut">
+        -- 5 most commented threads --
+      </section>
+      <section class="double-cut">
+        -- 5 most liked threads --
+      </section>
+    </main>
+  </div>
+</template>
+
+<script lang="ts">
+import Header from '@/components/layout/Header.vue'
+import ViewTitle from '@/components/layout/ViewTitle.vue'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'ThreadsHomeView',
+  components: {
+    Header,
+    ViewTitle
+  },
+  setup () {
+  }
+})
+</script>
