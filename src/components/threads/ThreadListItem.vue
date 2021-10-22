0390bf6b55807658fbc25e79cca30d66a519b1a3
Ville Takanen <vitkukissa@gmail.com>
2021-10-22 15:17:49 +0300

    fix: thread list item toolbat spacing

--- src/components/threads/ThreadListItem.vue
+++ src/components/threads/ThreadListItem.vue
@@ -11,7 +11,7 @@
     <Toolbar>
       <div>{{ thread.replyCount }}</div>
       <div>{{ thread.lovedCount }}</div>
-
+      <SpacerDiv />
       <AuthorTag :uid="thread.author" />
     </Toolbar>
   </section>
@@ -22,12 +22,13 @@ import { useMeta } from '@/state/meta'
 import { ThreadClass } from '@/state/threads/threads'
 import { computed, defineComponent } from 'vue'
 import AuthorTag from '../author/AuthorTag.vue'
+import SpacerDiv from '../layout/SpacerDiv.vue'
 import Toolbar from '../layout/Toolbar.vue'
 import Icon from '../material/Icon.vue'
 
 export default defineComponent({
   name: 'ThreadListItem',
-  components: { Icon, Toolbar, AuthorTag },
+  components: { Icon, Toolbar, AuthorTag, SpacerDiv },
   props: {
     thread: {
       type: ThreadClass,
