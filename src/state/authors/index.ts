e349f21097ff9edf1110f8d32107d9fa4bd56e67
Ville Takanen <vitkukissa@gmail.com>
2021-10-29 14:21:16 +0300

    fix: userlist subscribes for users

--- src/state/authors/index.ts
+++ src/state/authors/index.ts
@@ -3,50 +3,28 @@ import { ref, computed, ComputedRef } from 'vue'
 import { PublicProfile } from '@/state/authz'
 import { useMeta } from '../meta'
 import { collection, getFirestore, onSnapshot, DocumentData, getDoc, doc } from '@firebase/firestore'
+import { logEvent } from '@/utils/eventLogger'
 
-const authorsState = ref(new Array<PublicProfile>())
-const authors = computed(() => authorsState.value)
+const authors = computed(() => (Array.from(publicAuthorList.value.values())))
 const nonFrozenAuthors = computed(() => {
   const { frozen } = useMeta()
-  return authorsState.value.filter((a) => (!frozen.value.includes(a.uid || '')))
+  return authors.value.filter((a) => (!frozen.value.includes(a.uid || '')))
 })
-
-let init = false
-function _init () {
-  if (init) return
-  init = true
-
-  // fetch all authors
-  const profilesRef = collection(getFirestore(), 'profiles')
-  onSnapshot(profilesRef, (changes) => {
-    changes.docChanges().forEach((change) => {
-      const doc = change.doc
-      if (doc.exists()) {
-        const author: PublicProfile = {
-          uid: doc.id,
-          nick: doc.data()?.nick,
-          photoURL: doc.data()?.photoURL,
-          tagline: doc.data()?.tagline
-        }
-        authors.value.push(author)
-      }
-    })
-  })
-}
 
 export class AuthorClass {
   readonly uid:string
   readonly nick:string
+  readonly photoURL:string|undefined
 
   constructor (uid: string, data?:DocumentData) {
     this.uid = uid
     this.nick = data?.nick || 'N.N.'
+    this.photoURL = data?.photoURL || undefined
   }
 }
 const publicAuthorList = ref(new Map<string, AuthorClass>())
 
 async function fetchAuthor (uid: string): Promise<AuthorClass> {
-  console.debug('fetchAuthor', uid)
   const cached = publicAuthorList.value.get(uid)
   if (cached) return cached
 
@@ -67,6 +45,28 @@ async function fetchAuthor (uid: string): Promise<AuthorClass> {
   return new AuthorClass('')
 }
 
+let authorsSubscribed = false
+
+function subscribeToAuthors (): void {
+  if (authorsSubscribed) return
+  authorsSubscribed = true
+
+  logEvent('subscribeToAuthors')
+
+  onSnapshot(
+    collection(getFirestore(), 'profiles'),
+    (changes) => {
+      changes.docChanges().forEach((change) => {
+        if (change.type !== 'removed') {
+          publicAuthorList.value.set(change.doc.id, new AuthorClass(change.doc.id, change.doc.data()))
+        } else {
+          publicAuthorList.value.delete(change.doc.id)
+        }
+      })
+    }
+  )
+}
+
 function getAuthor (uid: string): PublicProfile {
   const profile = authors.value.find((p) => (p.uid === uid))
   if (profile) return profile
@@ -78,11 +78,11 @@ function getAuthor (uid: string): PublicProfile {
 }
 
 export function useAuthors (): {
-  authors: ComputedRef<PublicProfile[]>;
-  getAuthor: (uid: string) => PublicProfile;
+  authors: ComputedRef<AuthorClass[]>
+  getAuthor: (uid: string) => PublicProfile
   fetchAuthor: (uid: string) => Promise<AuthorClass>
-  nonFrozenAuthors: ComputedRef<PublicProfile[]>;
+  nonFrozenAuthors: ComputedRef<PublicProfile[]>
+  subscribeToAuthors: () => void
   } {
-  _init()
-  return { authors, getAuthor, nonFrozenAuthors, fetchAuthor }
+  return { authors, getAuthor, nonFrozenAuthors, fetchAuthor, subscribeToAuthors }
 }
