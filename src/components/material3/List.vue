3d7016e43ae9a1c5fd4cb1daac49621cc03ba8ea
Ville Takanen <vitkukissa@gmail.com>
2021-12-07 13:18:52 +0200

    fix: added material list styling comps

---
+++ src/components/material3/List.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="List">
+    <slot />
+  </div>
+</template>
+
+<style lang="sass" scoped>
+div.List
+  display: flex
+  flex-direction: column
+</style>
---
+++ src/components/material3/ListItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="ListItem">
+    <div class="ListItem-content">
+      <slot />
+    </div>
+    <SpacerDiv />
+    <div class="ListItem-action">
+      <slot name="action" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import SpacerDiv from '../layout/SpacerDiv.vue'
+
+export default defineComponent({
+  components: { SpacerDiv }
+})
+</script>
+
+<style lang="sass" scoped>
+div.ListItem
+  display: flex
+  box-sizing: border-box
+  padding: 12px 16px
+  line-height: 24px
+</style>
