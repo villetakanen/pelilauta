4257fc3ffe9a668378d384a0383b4b73033bdf49
Ville Takanen <vitkukissa@gmail.com>
2021-12-09 09:59:55 +0200

    fix: added site theme tool

---
+++ src/components/site/settings/SiteThemeTool.vue
@@ -0,0 +1,17 @@
+<template>
+  <Column class="SiteThemeTool">
+    <h1>{{ $t('site.themetool.title') }}</h1>
+  </Column>
+</template>
+
+<script lang="ts">
+import Column from '@/components/layout/Column.vue'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  components: { Column },
+  setup () {
+
+  }
+})
+</script>
